749b2e9a1f13685b37c51e692fb83be4a2604caf
lucaskaildev <lucaskaildev@gmail.com>
2025-09-20 22:38:26 -0300

    fix(db): initialize typeorm connection

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './database/database.module';
+import databaseConfig from './database/config/database-config';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(databaseConfig.getConfig()), DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
