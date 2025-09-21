d704a29fbee8df3c17653ea6b57a6994136ef035
lucaskaildev <lucaskaildev@gmail.com>
2025-09-21 00:03:11 -0300

    feat: global custom validation pipe

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
+import { CustomValidationPipe } from '@/pipes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new CustomValidationPipe()); // use validation pipe globally
+  app.setGlobalPrefix('api');
   await app.listen(process.env.BACKEND_PORT ?? 3000);
 }
 bootstrap();
