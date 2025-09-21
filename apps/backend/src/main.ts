59823be64fe2fda94399f3e8f8233c0efc205f81
lucaskaildev <lucaskaildev@gmail.com>
2025-09-21 20:36:26 -0300

    chore(cors): enable cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -4,7 +4,7 @@ import { CustomValidationPipe } from '@/pipes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.enableCors();
   app.useGlobalPipes(new CustomValidationPipe()); // use validation pipe globally
   app.setGlobalPrefix('api');
   await app.listen(process.env.BACKEND_PORT ?? 3000);
